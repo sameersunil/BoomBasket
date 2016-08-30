e104ee1b88804644880ebea22ce4f745210d6cb5
Sameer <sameer.sunil@in.tesco.com>
2016-08-30 07:02:15 +0530

    Postgres Working

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160824100049) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "orders", force: :cascade do |t|
     t.string   "email"
     t.float    "total"
