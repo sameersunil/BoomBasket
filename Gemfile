c665345df4f57ead5c0c04628f833ac89b7df6b0
sameersunil <sameersunil@in.tesco.com>
2016-09-12 15:38:02 +0530

    Done Elastic Search Basic

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'activerecord-insert_many'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'elasticsearch-model'
+gem 'elasticsearch-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 class Product < ApplicationRecord
+	include Elasticsearch::Model
+  	include Elasticsearch::Model::Callbacks
 	validates :name, :presence => true
 	validates :cat, :presence => true
 	validates :price, :presence => true
 	validates :img_url, :presence => true
 end
+
+Product.import	
