2c106ba4f3fa989c3e0a8255a6935d6c851b14e9
sameersunil <sameersunil@in.tesco.com>
2016-09-12 16:04:13 +0530

    Fixes adding same items to the cart!

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -26,8 +26,13 @@
 	end
 
   def addToCart
-    session[:count] = (session[:count].to_i + 1).to_s
-    session["I" + params[:prod]] = params[:prod]  + ":" + (verifyQty params[:cart][:qty])  
+    if session["I" + params[:prod]].nil?
+      session[:count] = (session[:count].to_i + 1).to_s
+      session["I" + params[:prod]] = params[:prod]  + ":" + (verifyQty params[:cart][:qty])
+    else
+      prevQty = session["I" + params[:prod]].split(":")[1]
+      session["I" + params[:prod]] = params[:prod]  + ":" + ((verifyQty params[:cart][:qty]).to_i + prevQty.to_i).to_s
+    end
     respond_to do |format|
       format.js
     end
