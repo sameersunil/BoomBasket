bc9398c7a7f75f3a10a8ef8bce236ece516d0a6b
sameersunil <sameersunil@in.tesco.com>
2016-09-13 04:09:33 +0530

    Revamped Homepage!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,17 @@
 class Product < ApplicationRecord
 	include Elasticsearch::Model
   	include Elasticsearch::Model::Callbacks
+
 	validates :name, :presence => true
 	validates :cat, :presence => true
 	validates :price, :presence => true
 	validates :img_url, :presence => true
+
+	def as_indexed_json(options={})
+    as_json(
+      only: [:name]
+      )
+  end
 end
 
 Product.import	
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 	#ADD the password encryption
 	validates :pwd, presence: true, length: { within: 6..40 }
 	validates :email, presence: true, format: { with: EMAIL_REG_EXP }, uniqueness: { case_sensitive: false }
-
+	
 	def self.authenticate user_email, user_pwd
 		user = User.find_by_email user_email
 		unless user.nil?
